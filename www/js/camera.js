36fd1bf0e451b7f3dd025d215d3c1976ff48f578
Alejandro Flores Villegas <AlejandroFV@mail.com>
2015-12-09 19:45:32 -0600

    Add the camera plugin

---
+++ www/js/camera.js
@@ -0,0 +1,37 @@
+var pictureSource;
+var destinationType;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady () {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+}
+
+function onPhotoDataSuccess () {
+    var smallImage = document.getElementById('smallImage');
+    smallImage.style.display = 'block';
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onPhotoURISuccess () {
+    var largeImage = document.getElementById('largeImage');
+    largeImage.style.display = 'block';
+    largeImage.src = imageURI;
+}
+
+function onFail (message) {
+    alert('Fallo al inicializar la camara: ' + message);
+}
+
+function capturePhoto () {
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: destinationType.DATA_URL });
+}
+
+function capturePhotoEdit () {
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL });
+}
+
+function getPhoto (source) {
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, destinationType: destinationType.FILE_URI, sourceType: source });
+}
